ea1a13b3cc7d67195818b270ab6e4d70f4ef849c
Tyler Redlev <tyler.redlev@gmail.com>
2020-06-29 18:03:38 +0300

    Word Guessing From Outer List files and folder created. Explanatory comments added to the file. Outer words list added to the folder.

---
+++ word_guessing_from_outer_document/word_guessing_from_outer_list.py
@@ -0,0 +1,22 @@
+"""
+Guessing a word from the list.
+Write a python code in the spyder software that may contain a couple of functions (number of functions in your program depends on yourself).
+The code allows user to guess the letters of the word that is randomly chosen from the given words_list.
+The words_list is uploaded on the Moodle and you need to import the list to your python code by defining a function.
+A sample output is given in the following.
+
+*---------------------------------------------
+The word contains 9 letters.
+Please enter one letter or a 9-letter word. A
+Yes! The word contains 2 "A"s!
+***A***A*
+*---------------------------------------------
+
+
+TO WRITE:
+1) FUNCTION WHICH WILL COUNT THE LETTER COUNT IN THE WORD
+2) ONE LETTER OR WORD INPUT ASKER
+3) YES CONTAINS NO DOESN'T CONTAIN
+4) HOW MANY TRIES COUNTER
+
+"""
